3b86c1476e2b3e0e2da6d74f8b80c7dc1c9b369f
Trina <92634994+trinayau@users.noreply.github.com>
2022-03-30 23:29:51 +0100

    added icons to homepage

--- Server/test/app.spec.js
+++ Server/test/app.spec.js
@@ -5,7 +5,7 @@ describe ('API Server', () => {
     let api;
     beforeAll(() => {
         api = app.listen(5000, () => {
-            console.log('Test server running at port 5000!')
+            console.log('Server running at port 5000!')
         }) //listen in on another port to not confuse matters. test server listening at 5000 hardcoded. 
     });
     afterAll((done) => { //just built in, passing it thru. we arent actually sending thru an api to test
